209124c0281f731baa9b2d72419a303cc87fe3e0
Ryan James <ryanmichaeljames@gmail.com>
2019-09-05 20:40:54 +1200

    Added contact page and specials.

--- assets/js/google-map.js
+++ assets/js/google-map.js
@@ -2,13 +2,20 @@
 var google;
 
 function init() {
+    if (!$('#map').length)
+        return;
+
+    var key = $('#map').data('key');
+    var latitude = $('#map').data('lat');
+    var longitude = $('#map').data('long');
+
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(latitude, longitude);
     // 39.399872
     // -8.224454
-    
+
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
         zoom: 7,
@@ -34,7 +41,7 @@ function init() {
         ]
     };
 
-    
+
 
     // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
@@ -42,11 +49,11 @@ function init() {
 
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['New York'];
+
+    var addresses = ['67 Aero Road, Valhalla, Centurion 0185, South Africa'];
 
     for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+        $.getJSON(`http://maps.googleapis.com/maps/api/geocode/json?key=${key}&address=${addresses[x]}&sensor=false`, null, function (data) {
             var p = data.results[0].geometry.location
             var latlng = new google.maps.LatLng(p.lat, p.lng);
             new google.maps.Marker({
@@ -57,6 +64,6 @@ function init() {
 
         });
     }
-    
+
 }
 google.maps.event.addDomListener(window, 'load', init);
